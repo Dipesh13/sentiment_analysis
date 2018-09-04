32fed43165a7847b46006487c8d0d994fad72a32
Dipesh13 <mangwani.dipesh@gmail.com>
2018-09-05 03:51:50 +0530

    universal sentence encoder

---
+++ sent_encoder.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import re
+import seaborn as sns
+import keras.layers as layers
+from keras.models import Model
+from keras import backend as K
+np.random.seed(10)
+
+module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/2" #@param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/2"]
+
+# Import the Universal Sentence Encoder's TF Hub module
+embed = hub.Module(module_url)
+
+# Compute a representation for each message, showing various lengths supported.
+word = "Elephant"
+sentence = "I am a sentence for which I would like to get its embedding."
+paragraph = (
+    "Universal Sentence Encoder embeddings also support short paragraphs. "
+    "There is no hard limit on how long the paragraph is. Roughly, the longer "
+    "the more 'diluted' the embedding will be.")
+messages = [word, sentence, paragraph]
+
+# Reduce logging output.
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+with tf.Session() as session:
+  session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+  message_embeddings = session.run(embed(messages))
+
+  for i, message_embedding in enumerate(np.array(message_embeddings).tolist()):
+    print("Message: {}".format(messages[i]))
+    print("Embedding size: {}".format(len(message_embedding)))
+    message_embedding_snippet = ", ".join(
+        (str(x) for x in message_embedding[:3]))
+    print("Embedding: [{}, ...]\n".format(message_embedding_snippet))
+
+# Compute a representation for each message, showing various lengths supported.
+messages = ["That band rocks!", "That song is really cool."]
+
+with tf.Session() as session:
+  session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+  message_embeddings = session.run(embed(messages))
+print(message_embeddings)
+
